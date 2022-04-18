57dc3ed09dc66d98f49bd345d6a4024a1f3e5fe2
Nicolle <nickkoetzler@gmail.com>
2022-04-18 14:55:53 -0300

    atualiza sistime, mantem online e envio de msg

--- scripts.js
+++ scripts.js
@@ -10,7 +10,14 @@ function pegarNome(){
     }; 
     requisicao = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", objetoNome);
     requisicao.catch(tratarErro); 
+    requisicao.then(inicializaSite);
 } 
+
+function inicializaSite(){
+    setInterval(carregarMensagens, 3000);
+    setInterval(verificaOnline, 4000);
+    carregarMensagens();
+}
 
 function tratarErro(erro){
     let statusCode = erro.response.status;
@@ -55,19 +62,41 @@ function mensagemNormal(mensagem){
     </div>
     `
 }
+/*
+function mensagemPrivada(mensagem){
+    document.querySelector(".mensagens").innerHTML += `
+    <div class="linha private">
+            <p>
+                <span class="horario">
+                    (${mensagem.time})
+                </span> <!-- separar texto -->
+                <span class="remetente">
+                    ${mensagem.from}
+                </span>
+                    reservadamente para
+                <span class="destinatario">
+                    ${mensagem.to}:
+                </span>
+                    ${mensagem.text}
+            </p>
+    </div>
+    `
+} */
 
 function adicionarMensagens(response){
     const dadosMensagens = response.data;
+    document.querySelector(".mensagens").innerHTML = ""
     for (let i = 0; dadosMensagens.length > i ;i++){
         if("status" === dadosMensagens[i].type){
             mensagemStatus(dadosMensagens[i]);
         } else if("message" === dadosMensagens[i].type){
             mensagemNormal(dadosMensagens[i]);
-        }
+        } /* else if("private_message" === dadosMensagens[i].type){
+            mensagemPrivada(dadosMensagens[i]);
+        } */
     } 
     document.querySelector(".linha:last-child").scrollIntoView()
 }
-carregarMensagens();
 
 function carregarMensagens(){
     promise = axios.get("https://mock-api.driven.com.br/api/v6/uol/messages");
@@ -79,5 +108,18 @@ function verificaOnline(){
 //    requisicao.catch();
 }
 
-
-//    setInterval(verificaOnline, 5000);
+function enviarMensagem(btn){
+    const textoMensagem = btn.parentNode.querySelector("input").value
+    btn.parentNode.querySelector("input").value = ""
+    const mensagem = {
+        from: nome,
+        to: "Todos",
+        text: textoMensagem,
+        type: "message"
+    }
+    const promisse = axios.post("https://mock-api.driven.com.br/api/v6/uol/messages", mensagem);
+    promisse.then(carregarMensagens);
+    promisse.catch(function(){
+        window.location.reload();
+    });
+}
